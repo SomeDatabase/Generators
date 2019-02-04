3385b1f73c78eb6e968a37c45c8734073c7819b8
SomeDatabase <38416172+SomeDatabase@users.noreply.github.com>
2019-02-04 16:22:18 -0500

    Create NameParser.py

---
+++ Names/NameParser.py
@@ -0,0 +1,33 @@
+#Name Parser
+#Programmed by Mary Sonnabend
+
+#import modules
+import csv
+
+#lists
+girl = []
+boy = []
+last=[]
+
+#open csv files for reading
+first = open('/first_names.csv')
+last = open('/last_names.csv')
+
+#open writing files
+g = open("girl.dat", "w")
+b = open("boy.dat", "w")
+l = open("last.dat", "w")
+
+#create csvreaders
+csvfirst = csv.reader(first)
+csvlast = csv.reader(last)
+
+#write girl and boy names to their respective files
+for row in csvfirst:
+    if row[1] == 'F':
+        g.write(row[0]+'\n')
+    else:
+        b.write(row[0]+'\n')
+for row in csvlast:
+    l.write(row[0].title()+'\n')
+print("Done!")
